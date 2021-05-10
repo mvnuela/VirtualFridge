e357df4b7c9a6dfad9a5029cabbce245d5ff7877
Crys <ysiu16@gmail.com>
2021-05-10 18:16:10 +0200

    nice

---
+++ app/src/main/java/com/am/virtualfridge/AddProductDialog.kt
@@ -0,0 +1,48 @@
+package com.am.virtualfridge
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDialog
+import com.am.virtualfridge.databinding.DialogAddProductBinding
+import java.util.*
+
+
+// tworze piekny dialog
+
+class AddProductDialog(context: Context) : AppCompatDialog(context) {
+
+    // dodaje rozne takie
+
+    private lateinit var binding: DialogAddProductBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DialogAddProductBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.tvAdd.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val amount = binding.etAmount.text.toString()
+
+            if(name.isEmpty()) {
+                Toast.makeText(context, "Please enter a name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(amount.isEmpty()) {
+                Toast.makeText(context, "Please enter quantity", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //robie zeby wszystko bylo z malej litery
+            val item = Product(name.toLowerCase(Locale.ROOT), amount.toInt())
+            MainActivity.addUpdateProduct(item)
+            dismiss()
+        }
+
+        binding.tvCancel.setOnClickListener {
+            cancel()
+        }
+    }
+}
