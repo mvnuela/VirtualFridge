401f67d057a70218af64349afdf1bcfb8a1a2e64
Cebula <patyczak.pat@vp.pl>
2021-06-01 14:27:36 +0200

    Implementation recycler view

--- app/src/main/java/com/am/virtualfridge/FavReceiptsAdapter.kt
+++ app/src/main/java/com/am/virtualfridge/FavReceiptsAdapter.kt
@@ -1,5 +1,6 @@
 package com.am.virtualfridge
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,12 +8,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.am.virtualfridge.fridge.AdapterFridge
+import com.am.virtualfridge.fridge.EditProductDialog
+import com.am.virtualfridge.fridge.Product
+import com.am.virtualfridge.receipts.Receipt
 
-class FavReceiptsAdapter : RecyclerView.Adapter<FavReceiptsAdapter.ViewHolder> (){
+class FavReceiptsAdapter (private val receiptList:ArrayList<Receipt>, val context: Context) : RecyclerView.Adapter<FavReceiptsAdapter.ViewHolder> (){
 
    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
     {
-
+        val nameOfReceipt: TextView = itemView.findViewById(R.id.receiptName)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,10 +25,12 @@ class FavReceiptsAdapter : RecyclerView.Adapter<FavReceiptsAdapter.ViewHolder> (
     }
 
     override fun getItemCount(): Int {
-        return 0
+        return receiptList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem=receiptList[position]
+        holder.nameOfReceipt.text=currentItem.name
 
     }
 }
--- app/src/main/java/com/am/virtualfridge/ReceiptsFragment.kt
+++ app/src/main/java/com/am/virtualfridge/ReceiptsFragment.kt
@@ -1,6 +1,7 @@
 package com.am.virtualfridge
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,19 +9,30 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.am.virtualfridge.fridge.AdapterFridge
+import com.am.virtualfridge.fridge.Product
+import com.am.virtualfridge.receipts.Receipt
 
 
 class ReceiptsFragment : Fragment() {
     lateinit var recycler: RecyclerView
-    var adapter: FavReceiptsAdapter = FavReceiptsAdapter()
+    private lateinit var listOfReceipts:ArrayList<Receipt>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_receipts, container, false)
         recycler = view.findViewById<RecyclerView>(R.id.recyclerViewReceipts)
         recycler.layoutManager = LinearLayoutManager(context)
-        recycler.adapter = adapter
         recycler.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         return view
+    }
+
+    private fun setupAdapter(arrayData: ArrayList<Receipt>){
+        try {
+            recycler.adapter = FavReceiptsAdapter(arrayData, context!!)
+        } catch (e: Exception) {
+            Log.d("Adapter", "Error")
+        }
+
     }
 }
